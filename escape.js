11884ba787aff9dc96c94eba0cc311b3a1f7f44a
santiecheg <104526400+santiecheg@users.noreply.github.com>
2022-07-29 13:13:36 -0300

    Add files via upload
    aniado pistas con mayor y menor

--- escape.js
+++ escape.js
@@ -1,15 +1,18 @@
 alert("Escape Room")
 let intentos=5;
 let vida=5;
-let incorrecto="";
-
-let actual=parseInt(prompt("Adivina un número de 1 a 100 para escapar"));
+let actual=101
+actual=parseInt(prompt("Adivina un número de 1 a 100 para escapar"));
 while (actual!=25){
-    if (actual=25){alert("Felicitaciones!! conseguiste adivinar y pudiste escapar");break};
+    
+    if (actual<=24){alert("oooops..."+" "+actual+" "+"No era, es un numero mas grande")};
+    if (actual>=26){alert("oooops..."+" "+actual+" "+"No era, es un numero mas chico")};
+    if (vida==1){alert("No conseguiste adivinar pero igualmente pudiste escapar");break}; 
+    if (actual==25){alert("Adivinaste, pudiste escapar");break}
+
     vida=vida-1;
     intentos=vida;
-    alert("ooops.. no era ese numero intenta de nuevo, te quedan"+" "+vida+" "+"intentos");
+    alert("Intenta de nuevo, te quedan"+" "+vida+" "+"intentos");
     actual=prompt("Bueno indica otro numero para intentar");
-    if (vida==1){alert("No conseguiste adivinar pero igualmente pudiste escapar");break}; 
     }
 alert("Eres libre");
