c901d496ada5d31928e03aa2d59cc59287202b3e
santiecheg <104526400+santiecheg@users.noreply.github.com>
2022-07-29 12:39:13 -0300

    Add files via upload

--- escape.js
+++ escape.js
@@ -5,12 +5,11 @@ let incorrecto="";
 
 let actual=parseInt(prompt("Adivina un número de 1 a 100 para escapar"));
 while (actual!=25){
+    if (actual=25){alert("Felicitaciones!! conseguiste adivinar y pudiste escapar");break};
     vida=vida-1;
     intentos=vida;
     alert("ooops.. no era ese numero intenta de nuevo, te quedan"+" "+vida+" "+"intentos");
     actual=prompt("Bueno indica otro numero para intentar");
- 
-    if (vida==1){alert("No conseguiste adivinar pero igualmente pudiste escapar");break};
-    if (actual==25){alert("Lo has conseguido, pudiste escapar");break}
+    if (vida==1){alert("No conseguiste adivinar pero igualmente pudiste escapar");break}; 
     }
 alert("Eres libre");
